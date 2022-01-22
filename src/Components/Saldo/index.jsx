f31b9754252410bb765d7ecd17aedd50f7292d74
eduardonk9999 <thiago.novak7@gmail.com>
2022-01-22 14:50:40 -0300

    Add, consegui colocar o valor total, pelo valor de saidas já sendo subtraido

--- src/Components/Saldo/index.jsx
+++ src/Components/Saldo/index.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 const Saldo = () => {
   const [valores, setValores] = useState([]);
+  const [valortotal, setValorTotal] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:8081/finances')
@@ -9,23 +10,24 @@ const Saldo = () => {
         return res.json()
       })
       .then(data => {
-        //const sum = data.reduce((acumulador, valoratual) => acumulador + Number(valoratual.valor), 0)
-      
-        setValores(data)
+        const sum = data.reduce((acumulador, valoratual) => acumulador + Number(valoratual.valor), 0)
+        setValores(sum)
+        setValorTotal(data)
       })
   }, []);
 
-  return(
-    <>
-       { 
-            valores.filter(entrada => entrada.categoria === 'Entrada')
-              .reduce((acc, item) => (
 
-                
-                console.log(item, acc)
-                    
-              ), 0)
-          }
+  const totaldeSaidas = valortotal.filter(saida => saida.categoria === 'Saida')
+  .reduce((acc, item) => acc + Number(item.valor), 0)
+
+
+  console.log(totaldeSaidas)
+  console.log(valores)
+
+
+  return( 
+    <>
+     <span>{(valores - totaldeSaidas)}</span>
     </>
   )
 }
